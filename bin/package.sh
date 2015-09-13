bdf911b7e75160d9cde44bce28dcac1d7b3f4ec8
teknopaul <teknopaul@gmail.com>
2015-09-14 00:29:49 +0200

    fix fail when repeating a build

--- bin/package.sh
+++ bin/package.sh
@@ -5,7 +5,9 @@
 cd $(dirname $0)
 cd ..
 
-VERSION=`grep version ../package.json | head -1 | cut '--delimiter=:' -f2 | tr -d ' ",'`
+VERSION=`grep version package.json | head -1 | cut '--delimiter=:' -f2 | tr -d ' ",'`
 
 tar cvf xgenjs-$VERSION.tar lib node_modules test jquery.xgen.js jquery.xgen.min-1.0.5.js package.json README.md xgen.jquery.json
+
+test -f xgenjs-$VERSION.tar.gz && rm xgenjs-$VERSION.tar.gz
 gzip xgenjs-$VERSION.tar
